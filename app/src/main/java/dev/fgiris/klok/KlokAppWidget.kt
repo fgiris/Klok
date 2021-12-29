670d37988e86528e932fb21357f812be82833c51
Fatih S <Suleyman.fatih.giris@dnb.no>
2021-12-29 14:33:57 +0100

    - Use actionStartActivity to launch an activity from a widget

--- app/src/main/java/dev/fgiris/klok/KlokAppWidget.kt
+++ app/src/main/java/dev/fgiris/klok/KlokAppWidget.kt
@@ -3,21 +3,20 @@ package dev.fgiris.klok
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.glance.GlanceModifier
+import androidx.glance.action.actionStartActivity
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
-import androidx.glance.appwidget.action.actionRunCallback
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
-import dev.fgiris.klok.action.LogTimeActionCallback
 
 class KlokAppWidget : GlanceAppWidget() {
     @Composable
     override fun Content() {
         Text(
             modifier = GlanceModifier.clickable(
-                actionRunCallback<LogTimeActionCallback>()
+                actionStartActivity<GlanceActionCallbackActivity>()
             ),
             text = "Hello from a Glance widget!",
             style = TextStyle(
